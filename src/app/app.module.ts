445d935f4ea8c4016aaad66ac554b4837c390202
codeup1054 <codeup1054@gmail.com>
2022-05-03 18:28:38 +0300

    2022-05-03 gpx-angular init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule
